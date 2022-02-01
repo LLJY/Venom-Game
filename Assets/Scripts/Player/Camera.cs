fe8aed71f40be9143ecf84b17b2e6dd1a55f3475
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2022-02-01 23:41:51 +0800

    Cleanup Character.cs
    - Remove Camera code from Character.cs

---
+++ Assets/Scripts/Player/Camera.cs
@@ -0,0 +1,75 @@
+using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class Camera: MonoBehaviour
+    {
+        // inspector assigned variables
+        [SerializeField] private GameObject player;
+        [SerializeField]private float cameraDampTime = 1;
+        [SerializeField]private float cameraOffsetFromPlayer;
+        [SerializeField]private Vector3 cameraAngles;
+
+
+        // runtime assigned variables
+        private Vector3 _cameraOffset;
+        private Vector3 _cameraCurrentVelocity;
+        private RaycastHit oldHit;
+        private Color oldHitColor;
+        
+        // cached variables
+        private int _wallLayerMask = 1 << 3;
+        private Transform _playerTransform;
+
+        private void Start()
+        {
+            _playerTransform = player.transform;
+            // let the player script set the camera angles from the player.
+            var playerPos = _playerTransform.position;
+        
+            transform.position = playerPos + new Vector3(0, 0, cameraOffsetFromPlayer);
+            transform.RotateAround(playerPos, new Vector3(0, 1, 0), cameraAngles.x);
+            transform.RotateAround(playerPos, new Vector3(1, 0, 0), cameraAngles.y);
+            transform.LookAt(playerPos);
+
+            _cameraOffset = transform.position - playerPos;
+        }
+        
+        private void LateUpdate()
+        {
+            // camera follow
+            var cameraPos = Vector3.SmoothDamp(transform.position, _cameraOffset + _playerTransform.position, ref _cameraCurrentVelocity,
+                cameraDampTime);
+            transform.position = cameraPos;
+        }
+        
+        private void FixedUpdate()
+        {
+            var playerPos = _playerTransform.position;
+            var ray = new Ray(playerPos,  transform.position - playerPos);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 10, _wallLayerMask))
+            {
+                if (oldHit.transform == null || hit.transform.gameObject.GetInstanceID() != oldHit.transform.gameObject.GetInstanceID())
+                {
+                    Debug.Log("hit!");
+                    var hitMaterial = hit.collider.gameObject.GetComponent<MeshRenderer>().material;
+                    oldHit = hit;
+                    oldHitColor = hitMaterial.color;
+                    hitMaterial.color = new Color(oldHitColor.r, oldHitColor.g, oldHitColor.b, 0.2f);
+                }
+            }
+            else
+            {
+                if (oldHit.transform != null)
+                {
+                    var hitMaterial = oldHit.transform.gameObject.GetComponent<MeshRenderer>().material;
+                    hitMaterial.color = oldHitColor;
+                    // unassign it to default
+                    oldHit = new RaycastHit();
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -7,28 +7,14 @@ using UnityEngine.Serialization;
 // TODO move camera logic into separate cam script
 public class Character : MonoBehaviour
 {
-    [FormerlySerializedAs("camera")] [SerializeField]private Camera mainCamera;
-    [SerializeField]private Vector3 cameraAngles;
-    [SerializeField]private float cameraOffsetFromPlayer;
     [SerializeField]private float playerSprintSpeed = 5;
-    [SerializeField]private float playerAcceleration = 2;
-    [SerializeField]private float cameraDampTime = 1;
     [SerializeField]private float characterJumpHeight = 2;
     
     // GetComponent assigned variables
     private CharacterController _characterController;
     private Animator _animator;
-    private Vector3 _cameraCurrentVelocity;
     private Vector3 _currentCharacterVelocity;
-    
-    // runtime assigned variables
-    private RaycastHit oldHit;
-    private Color oldHitColor;
-    private Vector3 _cameraOffset;
 
-    // cache some variables for performance reasons
-    private Transform _cameraTransform;
-    private int _wallLayerMask = 1 << 3;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -36,19 +22,6 @@ public class Character : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
-    void Start()
-    {
-        // let the player script set the camera angles from the player.
-        var transformPos = transform.position;
-        _cameraTransform = mainCamera.transform;
-        _cameraTransform.position = transformPos + new Vector3(0, 0, cameraOffsetFromPlayer);
-        _cameraTransform.RotateAround(transformPos, new Vector3(0, 1, 0), cameraAngles.x);
-        _cameraTransform.RotateAround(transformPos, new Vector3(1, 0, 0), cameraAngles.y);
-        _cameraTransform.LookAt(transformPos);
-
-        _cameraOffset = _cameraTransform.position - transformPos;
-    }
-    
     void Update()
     {
         HandleCharacterMovement();
@@ -89,40 +62,5 @@ public class Character : MonoBehaviour
         }
 
         _characterController.Move(_currentCharacterVelocity * Time.deltaTime);
-    }
-
-    private void LateUpdate()
-    {
-        // camera follow
-        var cameraPos = Vector3.SmoothDamp(_cameraTransform.position, _cameraOffset + transform.position, ref _cameraCurrentVelocity,
-            cameraDampTime);
-        _cameraTransform.position = cameraPos;
-    }
-
-    private void FixedUpdate()
-    {
-        var ray = new Ray(transform.position,  _cameraTransform.position - transform.position);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 10, _wallLayerMask))
-        {
-            if (oldHit.transform == null || hit.transform.gameObject.GetInstanceID() != oldHit.transform.gameObject.GetInstanceID())
-                {
-                    Debug.Log("hit!");
-                    var hitMaterial = hit.collider.gameObject.GetComponent<MeshRenderer>().material;
-                    oldHit = hit;
-                    oldHitColor = hitMaterial.color;
-                    hitMaterial.color = new Color(oldHitColor.r, oldHitColor.g, oldHitColor.b, 0.2f);
-                }
-        }
-        else
-        {
-            if (oldHit.transform != null)
-            {
-                var hitMaterial = oldHit.transform.gameObject.GetComponent<MeshRenderer>().material;
-                hitMaterial.color = oldHitColor;
-                // unassign it to default
-                oldHit = new RaycastHit();
-            }
-        }
     }
 }
