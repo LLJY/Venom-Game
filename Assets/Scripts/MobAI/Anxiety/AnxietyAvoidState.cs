cde84ca7784913e838e27e744b71b15c7387f992
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2022-02-07 17:30:09 +0800

    Implement simple MobAI for Anxiety Mob

---
+++ Assets/Scripts/MobAI/Anxiety/AnxietyAvoidState.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MobAI
+{
+    public class AnxietyAvoidState: State<AnxietyNpc>
+    {
+        private int _wallLayerMask = 1 << 3;
+        public AnxietyAvoidState(AnxietyNpc behaviour) : base(behaviour)
+        {
+        }
+
+        public override void Prepare()
+        {
+            Debug.Log("Anxiety Avoid State");
+        }
+
+        public override void CleanUp()
+        {
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        public override void LateUpdate()
+        {
+        }
+
+        public override void FixedUpdate()
+        {
+            /*
+             * In the event that the character is heading towards a wall, try to avoid it by incrementing
+             * the direction vector by 5 degrees and raycasting again.
+             */
+            var position = _behaviour.transform.position;
+            var direction = position - _behaviour._playerTransform.position;
+            var ray = new Ray(position, direction);
+            for (int i = 0; Physics.Raycast(ray, 5, _wallLayerMask) && i < 72; i++)
+            {
+                direction = Quaternion.Euler(0, 5 * i, 0) * direction;
+                ray = new Ray(position, direction);
+            }
+            _behaviour.agent.SetDestination(position + direction);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/MobAI/Anxiety/AnxietyNpc.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+namespace MobAI
+{
+    /// <summary>
+    /// Anxiety NPC will try to stay away from the player, it does not have any attack capability
+    /// However, it will give all nearby mobs a buff
+    /// </summary>
+    public class AnxietyNpc: StatefulMonoBehaviour<AnxietyNpc>
+    {
+        // inspector variables
+        public float baseBuffRadius = 5;
+        public float baseSpeed = 2;
+        public Animator animator;
+        public GameObject player;
+        public float avoidRadius = 5;
+
+        // cache variables for performance
+        [HideInInspector]public Transform _playerTransform;
+        
+        // runtime assigned varaibles
+        [HideInInspector]public NavMeshAgent agent;
+        
+        // NPC states
+        private AnxietyWanderState _wanderState;
+        private AnxietyAvoidState _avoidState;
+
+        // TODO randomize all values
+        public override void Awake()
+        {
+            base.Awake();
+            // we are not using a manager, override the manager settings
+            enabled = true;
+            animator = GetComponent<Animator>();
+            
+            _wanderState = new AnxietyWanderState(this);
+            _avoidState = new AnxietyAvoidState(this);
+        }
+
+        private void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.speed = baseSpeed;
+            CurrentState = _wanderState;
+
+            _playerTransform = player.transform;
+        }
+
+        public override void Update() {
+            CurrentState.Update();
+            var walkSpeed = agent.velocity.magnitude;
+            var horizontalSpeed = agent.velocity.x;
+            animator.SetBool("Walking", walkSpeed > 0);
+            animator.SetFloat("Walking Speed", walkSpeed);
+
+            // ANYSTATE transitions
+
+            if (Vector3.Distance(transform.position, _playerTransform.position) < avoidRadius)
+            {
+                if (CurrentState != _avoidState)
+                {
+                    CurrentState = _avoidState;
+                }
+            }
+            else if (CurrentState != _wanderState)
+            {
+                CurrentState = _wanderState;
+            }
+        }
+        
+    }
+}
+
