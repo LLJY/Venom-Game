b14b7dc78571b4fdd9fb387b6f828dee6880cf97
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2022-02-17 23:21:29 +0800

    Door: simplify the positioning logic significantly

--- Assets/Scripts/World/Door.cs
+++ Assets/Scripts/World/Door.cs
@@ -21,28 +21,17 @@ namespace World
         // without this, the trigger may trigger more than once, causing the game to freak out
         private bool _exit = false;
 
-        void Update()
+        private void Start()
         {
-            /*
-             * The world script sets the scale in Start() and there is a real possibility that
-             * the scale is set BEFORE we get the opportunity to update the door position
-             * update it here after all the scripts have done initializing and then disable the script
-             * to prevent further updates.
-             *
-             * Set the door's x position such that is is near the adjacent door, so it forces the player to traverse the entire map
-             * from one door to the other instead of just walking straight.
-             * Raycast towards the wall that the door is supposed to be at, then set the coordinates to the hit point
-             * this ensures that the door is always on the SURFACE of the wall and not inside of it.
-             */
-            var wallPos = adjacentWall.transform.position;
-            var doorPos =
-                new Vector3( Mathf.Sign(wallPos.x)*-10+wallPos.x, transform.position.y, transform.position.z);
-            var wallLayerMask = 1 << 3;
-            RaycastHit hit;
-            var ray = new Ray(doorPos, wall.transform.position - transform.position);
-            Physics.Raycast(ray, out hit, 1000f, wallLayerMask);
-            transform.position = new Vector3(doorPos.x, doorPos.y, hit.point.z) + transform.up * -0.1f;
+            SetDoorPosition();
             enabled = false;
+        }
+
+        private void SetDoorPosition()
+        {
+            var adjacentPos = adjacentWall.transform.position;
+            var wallPos = wall.transform.position;
+            transform.position = new Vector3(Mathf.Sign(adjacentPos.x) * -10 + adjacentPos.x, 0, Mathf.Sign(wallPos.z) * -1.1f + wallPos.z);
         }
 
         // Triggers are unaffected by enable, so we can still play the door anim
